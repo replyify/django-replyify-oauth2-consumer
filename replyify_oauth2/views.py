2132ff8cdd75ee79017dfdb7e6b568f2306b43b4
Francis Roch <francis@manafont.net>
2016-09-27 10:34:01 -0400

    store and validate 'state' between /authorize and /callback;  if mismatch, abort

--- replyify_oauth2/views.py
+++ replyify_oauth2/views.py
@@ -6,11 +6,14 @@
 # file 'LICENSE.md', which is part of this source code package.
 #
 
+import os
+import redis
 import requests
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.utils.crypto import get_random_string
+
 import _settings
 
 
@@ -18,36 +21,68 @@ def index(request):
     return HttpResponse("Replyify-OAuth2 index.")
 
 
+def _get_uid(request=None):
+    if hasattr('guid', request.user):
+        uid = request.user.guid
+    else:
+        uid = request.user.id
+    return uid
+
+
+def _get_redis_connection(host=None, port=None, passwd=None):
+    if not (host and port):
+        redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
+        tokenized = redis_url.split(':')
+        host = tokenized[1].lstrip('//')
+        port = tokenized[2]
+    try:
+        r = redis.Redis(
+            host=host,
+            port=port,
+            password=passwd
+        )
+        return r
+    except:
+        return None
+
+
 @login_required
 def authorize(request):
-    uid = request.user.guid
-    client_id = settings.REPLYIFY_CLIENT_ID
-    redirect_uri = settings.REPLYIFY_REDIRECT_URI
+    uid = _get_uid(request)
+    client_id = _settings.REPLYIFY_CLIENT_ID
+    redirect_uri = _settings.REPLYIFY_REDIRECT_URI
     response_type = 'code'
 
-    # TODO(froch): store state in redis with user.id
     state = get_random_string(20, "abcdefghijklmnopqrstuvwxyz0123456789")
+    r = _get_redis_connection()
+    if r is not None:
+        r.set(state, uid)
 
     args = "client_id={0}&redirect_uri={1}&response_type={2}&state={3}".format(client_id, redirect_uri, response_type, state)
-    url = "{0}?{1}".format(settings.REPLYIFY_AUTH_URL, args)
+    url = "{0}?{1}".format(_settings.REPLYIFY_AUTH_URL, args)
 
     return redirect(url)
 
 
 def callback(request):
+    uid = _get_uid(request)
 
-    # TODO(froch): validate state with one saved above.
     state = request.GET['state']
+    r = _get_redis_connection()
+    if r is not None:
+        from_redis = r.get(state)
+        if from_redis != uid:
+            raise Exception("Something fishy is happening. Abort ...")
 
     code = request.GET['code']
     data = {
         'grant_type': 'authorization_code',
         'code': code,
-        'client_id': settings.REPLYIFY_CLIENT_ID,
-        'redirect_uri': settings.REPLYIFY_REDIRECT_URI
+        'client_id': _settings.REPLYIFY_CLIENT_ID,
+        'redirect_uri': _settings.REPLYIFY_REDIRECT_URI
     }
 
-    url = settings.REPLYIFY_TOKEN_URL
+    url = _settings.REPLYIFY_TOKEN_URL
     r = requests.post(url=url, data=data)
 
     return HttpResponse(str(request))
