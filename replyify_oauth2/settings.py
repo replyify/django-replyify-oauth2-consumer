f6cf0dee5e5627cdfcd9e339083eba5ef3844c1a
Marco DiDomenico <marco@sociagram.com>
2022-08-04 10:02:59 -0400

    add some tips to settings file

--- replyify_oauth2/settings.py
+++ replyify_oauth2/settings.py
@@ -8,12 +8,15 @@
 from __future__ import unicode_literals
 from django.conf import settings
 
+# These credentials can be generated and retrieved from your Replyify account where the `OAuth2 Application` was created/managed
 REPLYIFY_CLIENT_ID = getattr(settings, 'REPLYIFY_CLIENT_ID', None)
 REPLYIFY_CLIENT_SECRET = getattr(settings, 'REPLYIFY_CLIENT_SECRET', None)
+# This is the path to your oauth callback view: i.e. https://youapplication.com/replyify/callback
+# This path must be set in your `OAuth2 Application Settings` in Replyify and must match
 REPLYIFY_REDIRECT_URI = getattr(settings, 'REPLYIFY_REDIRECT_URI', None)
 
 REPLYIFY_AUTH_URL = getattr(settings, 'REPLYIFY_AUTH_URL', 'https://app.replyify.com/oauth2/authorize')
 REPLYIFY_TOKEN_URL = getattr(settings, 'REPLYIFY_TOKEN_URL', 'https://app.replyify.com/oauth2/token')
 
-REPLYIFY_USER_ID_FIELD = getattr(settings, 'REPLYIFY_USER_ID_FIELD', 'id')
-REPLYIFY_DENIED_REDIRECT = getattr(settings, 'REPLYIFY_DENIED_REDIRECT', '/')
+REPLYIFY_USER_ID_FIELD = getattr(settings, 'REPLYIFY_USER_ID_FIELD', 'id')  # Foreign Key used by your application for referencing users in your db
+REPLYIFY_DENIED_REDIRECT = getattr(settings, 'REPLYIFY_DENIED_REDIRECT', '/')  # Fallback path for denied access
